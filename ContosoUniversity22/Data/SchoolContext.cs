9cbfb6274712333b63c872f5284f10479bf030c2
Pawe Szczeszek <pszczesz@gmail.com>
2019-03-11 14:30:41 +0100

    first

---
+++ ContosoUniversity22/Data/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using ContosoUniversity22.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity22.Data {
+    public class SchoolContext : DbContext 
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
---
+++ ContosoUniversity22/Models/Enrollment.cs
@@ -0,0 +1,13 @@
+﻿namespace ContosoUniversity22.Models {
+    public enum Grade {
+        A,B,C,D,E
+    }
+    public class Enrollment {
+        public int EnrollmentID { get; set; }
+        public int CurseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
