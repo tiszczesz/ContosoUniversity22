7eb9ebdf72a33a4e7b74a6808b354e4cbcd604e8
Pawe Szczeszek <pszczesz@gmail.com>
2019-04-16 10:57:39 +0200

    many_to_many_update

---
+++ ContosoUniversity22/Models/SchoolViewModels/AssignedCourseData.cs
@@ -0,0 +1,7 @@
+﻿namespace ContosoUniversity22.Models.SchoolViewModels {
+    public class AssignedCourseData {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
